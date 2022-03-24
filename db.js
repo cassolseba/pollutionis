bda942f8e6c2fae67d14e58bca931ed25f58467a
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2022-03-24 18:11:22 +0100

    Added DB URI env variable

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
+const { connectionString } = require('pg/lib/defaults');
+
 const Pool = require('pg').Pool;
 require('dotenv').config();
+connectionString = process.env.DB_URI;
 
 const pool = new Pool ({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DATABASE_PORT, 
-    database: process.env.DATABASE
+    connectionString,
 })
 
 module.exports = pool;
